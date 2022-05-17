f00669d8a1b2940bab207857e50e5ec310b5c081
ahmtataman <ahmtataman@gmail.com>
2022-05-18 00:43:47 +0300

    displayname

--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -20,6 +20,7 @@ export default class Login extends Component {
   constructor() {
     super();
     this.state = {
+      displayname: '',
       email: '',
       password: '',
       isLoading: false,
